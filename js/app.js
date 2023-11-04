639188c9f30777944eedb5e471162d1c1732db4b
mirza-azam <mirzaazam884@gmail.com>
2023-11-04 23:34:08 +0600

    added clear and backspace

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+// create actual 4 digit value 
 function getPin() {
     const pin = generatePin();
-    const pinString = pin + '';
+    const pinString = pin + ''; // emty string use for number to string convert
 
     if (pinString.length == 4) {
         return pin;
@@ -10,15 +11,42 @@ function getPin() {
     }
 }
 
+//generate random 4 digit value
 function generatePin() {
     const random = Math.round(Math.random() * 10000);
     return random;
 }
 
-document.getElementById('generate-pin').addEventListener('click', function(){
+// click generate btn than generate pin
+document.getElementById('generate-pin').addEventListener('click', function () {
     const generateField = document.getElementById('generate-field');
     const pinGet = getPin();
     generateField.value = pinGet;
-
-    console.log('clicked');
 })
+
+
+document.getElementById('buttons').addEventListener('click', function () {
+    const getNumberFromBtn = event.target.innerText;
+    const typeNumber = document.getElementById('typed-inputField');
+    const getImidiateNumberFromTypeNumberField = typeNumber.value;
+
+    if (isNaN(getNumberFromBtn)) {
+        if (getNumberFromBtn == '<') {
+            const digit = getImidiateNumberFromTypeNumberField.split('');
+            digit.pop();
+            const digitremain = digit.join('');
+            typeNumber.value = digitremain;
+        }
+    }
+    else {
+        const previousNumber = getImidiateNumberFromTypeNumberField + getNumberFromBtn;
+        typeNumber.value = previousNumber;
+    }
+
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+    const clear = document.getElementById('typed-inputField');
+    clear.value = '';
+});
+
